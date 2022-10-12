59199e9dfa108ba27ad9720eb4a999ef48276323
RichardChiqui <59772504+RichardChiqui@users.noreply.github.com>
2022-10-11 23:30:16 -0400

    started files for twitter clone

---
+++ my-app/src/LeftOptions.js
@@ -0,0 +1,17 @@
+
+import React from "react"
+
+
+export default function LeftOptions(props){
+   const style = { font}
+
+    return(
+       <div>
+
+          {props.icon}  {props.text}
+
+
+       </div>
+
+    )
+}
