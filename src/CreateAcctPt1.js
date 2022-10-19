462268eb37a652a5a1dfd65545b474a0d3caffd0
PROMETHEUSGROUP\rchiqui <rchiqui@prometheusgroup.com>
2022-10-19 14:32:56 -0400

    finsihed login page, begun work on creating account process.

---
+++ src/CreateAcctPt1.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function CreatAcctpt1(props){
+
+
+    return(
+<div className="create-acct-section" style={props.style}>
+    hello
+    </div>
+
+
+    )
+}
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,19 +1,53 @@
 import React from "react"
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+
+import CreatAcctpt1 from "./CreateAcctPt1";
+
 
 export default function LoginPage(){
-    return(<div className="login">
-        <div className="blue-divider"> 
-        </div>
 
-        <div className="login-info">
-        <button type="button" className="enter-btn"> Sign up</button>
-        <button type="button" className="enter-btn"> Log in </button>
+    const [show, setShow] = React.useState(false)
 
+    const styles= {display: show? "none":"Block"}
+    const blurStyle = {filter: show? "blur(2px)":"blur(0px)"}
+
+    function createAccount(){
+        setShow(!show)
+    }
+    console.log(styles)
+    return(
+    
+        <div className="login" >
+                <div className="blue-divider" style={blurStyle}> 
+                </div>
+               
+
+<CreatAcctpt1 style={styles}/>
+        <div className="login-info" style={blurStyle}> 
+            
+        <div className="login-container">
+        <img src="https://img.icons8.com/color/48/000000/twitter--v1.png"  />
+   
+            <h1>See what's happening in the world right now</h1>
+            <h2>Join Twitter Today.</h2>
+
+            <div className="login-btns">
+            
+                <button type="button" onClick={createAccount} className="signup-btn" > Sign up</button>
+
+                <button type="button" className="login-btn"> Log in </button>
+            </div>
         </div>
+            </div>
 
         
-        
-    </div>
+
+                
+
+                
+                
+            </div>
+    
+            
     )
 }
